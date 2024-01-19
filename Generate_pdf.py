5f4d8452541d5a4cb1f5c567379bbc6e8e40a0f2
Remy <remy.deboni@gmail.com>
2024-01-19 17:28:43 +0100

    Update code

--- Generate_pdf.py
+++ Generate_pdf.py
@@ -12,108 +12,35 @@ from fpdf import FPDF
 from datetime import datetime
 import os
 # Get the current date and time
-current_date_time = datetime.now().strftime("%Y-%m-%d")
+current_date_time = datetime.now().strftime("%Y-%m")
 
 
 
-def load_CAS(file):
+def load_CID(file):
     
     with open(file, "r", encoding="unicode_escape") as file:
-        names_list = list(filter(None, (line.rstrip() for line in file)))
-    return(names_list)
+        CID_list = list(filter(None, (line.rstrip() for line in file)))
+    return(CID_list)
 
-def download_image(names_list):
-    list_name = []
+def download_image(CID_list):
 
-    for name in names_list:
+    for CID in CID_list:
         # Construct the URL for the chemical search
-        search_url = 'https://www.chemspider.com/Search.aspx?q=' + str(name)
-        print(f"Searching for: {name}")
+        search_url = 'https://pubchem.ncbi.nlm.nih.gov/image/imgsrv.fcgi?cid=' + str(CID)+"&t=l"
+        print(f"Searching for: {CID}")
 
         # Make a request to the search URL
         search_response = requests.get(search_url)
 
         if search_response.status_code == 200:
             # Parse the HTML content using BeautifulSoup
-            try:
-                soup = BeautifulSoup(search_response.text, 'html.parser')
-
-                # Extract the name
-                extracted_name = soup.find('span', {'id': 'ctl00_ctl00_ContentSection_ContentPlaceHolder1_RecordViewDetails_rptDetailsView_ctl00_WrapTitle'}).text.strip()
-                list_name.append(extracted_name)
-
-                # Find the img tag with the specified ID
-                img_tag = soup.find('img', {'id': 'ctl00_ctl00_ContentSection_ContentPlaceHolder1_RecordViewDetails_rptDetailsView_ctl00_ThumbnailControl1_viewMolecule'})
-                # Extract the source (src) attribute of the img tag
-                relative_image_url = img_tag['src']
-
-                # Create an absolute URL based on the base URL of the page
-                absolute_image_url = urljoin("https://www.chemspider.com", relative_image_url)
-
-                # Make a request to the absolute image URL
-                image_response = requests.get(absolute_image_url)
-
-                # Check if the image request was successful (status code 200)
-                if image_response.status_code == 200:
-                    # Save the image to a file
-                    image_path = os.path.join("image", f"{name}.jpg")
+                    image_path = os.path.join("image", f"{CID}.png")
                     with open(image_path, 'wb') as file:
-                        file.write(image_response.content)
+                        file.write(search_response.content)
                         print('Image downloaded successfully.')
-                else:
-                    print(f'Failed to download image. Status code: {image_response.status_code}')
+        else:
+             print(f'Failed to download image. Status code: {search_response.status_code} \n Verify CID: {CID}')
 
-            except Exception as e:
-                print(f'Error parsing search response: {e}')
-
-                # If the initial search fails, try the multiple search
-                print("Trying multiple search for this CAS")
-                soup = BeautifulSoup(search_response.text, 'html.parser')
-
-                first_link = soup.find('tbody').find('tr').find('td').find('a')
-
-                if first_link:
-                    url = first_link['href']
-
-                    # Make a request to the selected URL
-                    response = requests.get('https://www.chemspider.com' + url)
-
-                    if response.status_code == 200:
-                        # Parse the HTML content using BeautifulSoup
-                        try:
-                            soup = BeautifulSoup(response.text, 'html.parser')
-
-                            # Extract the name
-                            extracted_name = soup.find('span', {'id': 'ctl00_ctl00_ContentSection_ContentPlaceHolder1_RecordViewDetails_rptDetailsView_ctl00_WrapTitle'}).text.strip()
-                            list_name.append(extracted_name)
-
-                            # Find the img tag with the specified ID
-                            img_tag = soup.find('img', {'id': 'ctl00_ctl00_ContentSection_ContentPlaceHolder1_RecordViewDetails_rptDetailsView_ctl00_ThumbnailControl1_viewMolecule'})
-                            # Extract the source (src) attribute of the img tag
-                            relative_image_url = img_tag['src']
-
-                            # Create an absolute URL based on the base URL of the page
-                            absolute_image_url = urljoin("https://www.chemspider.com", relative_image_url)
-
-                            # Make a request to the absolute image URL
-                            image_response = requests.get(absolute_image_url)
-
-                            # Check if the image request was successful (status code 200)
-                            if image_response.status_code == 200:
-                                # Save the image to a file
-                                image_path = os.path.join("image", f"{name}.jpg")
-                                with open(image_path, 'wb') as file:
-                                    file.write(image_response.content)
-                                    print('Image downloaded successfully.')
-                            else:
-                                print(f'Failed to download image. Status code: {image_response.status_code}')
-
-                        except Exception as e:
-                            print(f'Error parsing response: {e}')
-                    else:
-                        print(f'Failed to make request to multiple search URL. Status code: {response.status_code}')
-
-    return list_name
 
 # Example usage
 
@@ -124,7 +51,7 @@ def Print_Cell(x,y):
 def Create_sticker(x,y,n):
 #Image
     pdf.set_xy(x,y)
-    image_path = os.path.join("image", CAS_list[n] + ".jpg")
+    image_path = os.path.join("image", CID_list[n] + ".png")
     pdf.image(image_path,w=0.5,h=0.5)
     
     
@@ -133,29 +60,39 @@ def Create_sticker(x,y,n):
 
     
 
-    #CAS
+    #CID
     pdf.set_xy(x+0.5,y+0.5/4)
-    pdf.cell(2,0.5/4, text ="CAS: "+CAS_list[n])
+    pdf.cell(2,0.5/4, text ="CID: "+CID_list[n])
 
     #DATE
     pdf.set_xy(x+0.5,y+2*0.5/4)
     pdf.cell(2,0.5/4, text ="DATE: "+str(current_date_time))
+    
+    #position 
+    #rangement 
+    pdf.set_xy(x+1.25,y+2*0.5/4)
+    pdf.cell(2,0.5/4, text ="position ID:")
+   
 
     #ppm
-    pdf.set_xy(x+0.5,y+3*0.5/4)
+    pdf.set_xy(x+0.5,y+3*0.5/4-0.025)
     pdf.cell(2,0.5/4, text ="ppm:")
+    
+    
+
 
 x_pos = [0.47,2.97,5.47] #inch 
 y_pos = [0.5,1,1.5,2,2.5,3,3.5,4,4.5,5,5.5,6,6.5,7,7.5,8,8.5,9,9.5,10] #inch
-file ="CAS_list.txt"
-CAS_list = load_CAS(file)
-#name_molecule = download_image(CAS_list)
+file ="CIDlist.txt"
+CID_list = load_CID(file)
+download_image(CID_list)
 
 
 
-value = len(CAS_list)
+value = len(CID_list)
 if value > 60:
     value = 60
+    print("print only first 60 compounds")
 skip = 0
 n=0
 compteur =0
@@ -176,7 +113,7 @@ while n < value:
             if n == value:
                 break
             if compteur>=skip:
-                Create_sticker(x+0.3,y,n)
+                Create_sticker(x+0.1,y,n)
                 n+=1
                
 
@@ -184,8 +121,8 @@ while n < value:
 for y in y_pos:
     for x in x_pos:
         Print_Cell(x,y)
-
 """
+
 
 pdf.output("new1.pdf")
 
